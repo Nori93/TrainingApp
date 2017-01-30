976218723ffee481ae4bc6a53af55c865a5a04bf
norbert <airdarkangel1@gmial.com>
2017-01-30 18:55:39 +0100

    Buttony zmiana kolorów

--- app/src/main/java/com/example/norbert/myapplication/Gui/Fragments/AddTrainingWindow.java
+++ app/src/main/java/com/example/norbert/myapplication/Gui/Fragments/AddTrainingWindow.java
@@ -41,7 +41,6 @@ public class AddTrainingWindow extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_add_training,container, false);
     }
 
